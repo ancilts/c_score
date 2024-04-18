c89681a739ebfaf217860a5058f00c1723dde2e7
ancilts <147413580+ancilts@users.noreply.github.com>
2024-04-18 16:36:52 +0000

    b

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pickle
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def hello():
     return render_template('index.html')
@@ -13,22 +14,23 @@ def predict():
         Age = float(request.form['Age'])
         Annual_Income = float(request.form['Annual_Income'])
         Num_of_Delayed_Payment = float(request.form['Num_of_Delayed_Payment'])
-        Num_Bank_Accounts = float(request.form['Total_Num_Credit_Products'])
+        Num_Bank_Accounts = float(request.form['Num_Bank_Accounts'])
         Outstanding_Debt = float(request.form['Outstanding_Debt'])
 
         features = [[Age , Annual_Income , Num_of_Delayed_Payment , Num_Bank_Accounts ,Outstanding_Debt]]
-        print(features)
+        
         model = pickle.load(open('model_dtc.pkl', 'rb'))
+        
             
         prediction = model.predict(features)
-        print(prediction)
+        
         
         mapping = {0: 'GOOD', 1: 'POOR', 2: 'STANDARD'}
         predicted_result = mapping[prediction[0]]
-        print(predicted_result)
+        
 
     return render_template("prediction.html", result=predicted_result)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
